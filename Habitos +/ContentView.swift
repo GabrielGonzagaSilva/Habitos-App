1d80cf77f8dee04430a345d534b9b2c843ae374c
Gabriel Gonzaga <gabrielgonzagasilva@outlook.com>
2024-09-21 02:09:45 -0300

    Initial Commit

---
+++ Habitos +/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Habitos +
+//
+//  Created by Gabriel Gonzaga  on 21/09/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Habitos +/Habitos__App.swift
@@ -0,0 +1,17 @@
+//
+//  Habitos__App.swift
+//  Habitos +
+//
+//  Created by Gabriel Gonzaga  on 21/09/24.
+//
+
+import SwiftUI
+
+@main
+struct Habitos__App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
